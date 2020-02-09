a5ca134f143deb01417e42336d59552f4c69ff1c
Stuart Rackham <srackham@gmail.com>
2020-02-09 17:41:33 +1300

    utils: glob() accepts multiple patterns and excludes directories.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,12 +1,10 @@
-import { walkSync } from "https://deno.land/std/fs/mod.ts"
-import { globToRegExp } from "https://deno.land/std/path/mod.ts"
+import { walkSync } from "https://deno.land/std/fs/mod.ts";
+import { globToRegExp } from "https://deno.land/std/path/mod.ts";
 
-// Return array of file names matching the glob pattern relative to the cwd.
-export function glob(pattern: string, options?): string[] {
-  let regex = globToRegExp(pattern);
-  let iter = walkSync(".", { match: [regex] });
+// Return array of file names matching the glob patterns relative to the cwd.
+// e.g. glob("tmp/*.ts", "lib/*.ts", "mod.ts");
+export function glob(...patterns: string[]): string[] {
+  let regexps = patterns.map(pat => globToRegExp(pat));
+  let iter = walkSync(".", { match: regexps, includeDirs: false });
   return Array.from(iter, info => info.filename);
 }
-
-let files = glob("**/*.ts");
-console.log(files);
