1d1f35a4da25fe06244292fea81b4677763dce07
Stuart Rackham <srackham@gmail.com>
2020-02-11 12:18:31 +1300

    Prefer `===` to `==`

--- lib/cli.ts
+++ lib/cli.ts
@@ -8,22 +8,22 @@ function parseArgs(args: string[], env: Env): void {
     const match = arg.match(/^([\w_]+)=(.*)$/);
     if (match) {
       env[match[1]] = match[2];
-    } else if (arg == "-f" || arg == "--force") {
+    } else if (arg === "-f" || arg === "--force") {
       env["-f"] = true;
       env["--force"] = true;
-    } else if (arg == "-h" || arg == "--help") {
+    } else if (arg === "-h" || arg === "--help") {
       env["-h"] = true;
       env["--help"] = true;
-    } else if (arg == "-l" || arg == "--list") {
+    } else if (arg === "-l" || arg === "--list") {
       env["-l"] = true;
       env["--list"] = true;
-    } else if (arg == "-n" || arg == "--dry-run") {
+    } else if (arg === "-n" || arg === "--dry-run") {
       env["-n"] = true;
       env["--dry-run"] = true;
-    } else if (arg == "-q" || arg == "--quiet") {
+    } else if (arg === "-q" || arg === "--quiet") {
       env["-q"] = true;
       env["--quiet"] = true;
-    } else if (arg == "--version") {
+    } else if (arg === "--version") {
       env["--version"] = true;
     } else {
       env.tasks.push(arg);
