34abe7f001c4bf133cd63388b774b654fbacc2d3
Stuart Rackham <srackham@gmail.com>
2020-02-11 15:23:52 +1300

    test: Added async task to Drakefile.ts.

--- Drakefile.ts
+++ Drakefile.ts
@@ -1,18 +1,24 @@
-import { desc, run, task } from './mod.ts'
+import { desc, run, task } from "./mod.ts";
 
-desc("Task 1")
-task("1", ["2", "3"], function () {
-    console.log("task: 1")
-})
+desc("Task 1");
+task("1", ["2", "3"], function() {
+  console.log("task: 1");
+});
 
 // desc("Task 2")
-task("2", ["3"], function () {
-    console.log("task: 2")
-})
+task("2", ["3"], function() {
+  console.log("task: 2");
+});
 
-desc("Task 3")
-task("3", [], function () {
-    console.log("task: 3")
-})
+desc("Task 3");
+task("3", [], async function() {
+  console.log("task: 3");
+  await new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, 1000);
+  });
+  console.log("done");
+});
 
-run()
+run();
