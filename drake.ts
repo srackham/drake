bd5d6fad82324388625b82755355839c791136cc
Stuart Rackham <srackham@gmail.com>
2020-02-23 20:00:51 +1300

    Move `--help` and `--version` processing from `run()` API to top-level of `mod.ts`. Move `--directory` processing from `drake.ts` to  top-level of `mod.ts`. These changes allow a drakefile to execute`--directory`, `--help` and `--version` options.

--- drake.ts
+++ drake.ts
@@ -1,29 +1,12 @@
 // A thin CLI wrapper for the Drake library module.
 
 import { existsSync } from "https://deno.land/std@v0.33.0/fs/mod.ts";
-import * as path from "https://deno.land/std@v0.33.0/path/mod.ts";
-import { help } from "./lib/help.ts";
-import { abort } from "./lib/utils.ts";
-import { env, vers } from "./mod.ts";
+import { abort, env } from "./mod.ts";
 
-if (env["--help"]) {
-  help();
-} else if (env["--version"]) {
-  console.log(vers);
-} else {
-  let drakefile = env["--drakefile"] ? env["--drakefile"] : "./Drakefile.ts";
+if (!env["--help"] && !env["--version"]) {
+  const drakefile = env["--drakefile"];
   if (!existsSync(drakefile) || !Deno.statSync(drakefile).isFile()) {
     abort(`--drakefile missing or not a regular file: ${drakefile}`);
   }
-  if (env["--directory"]) {
-    const dir = env["--directory"];
-    if (!existsSync(dir) || !Deno.statSync(dir).isDirectory()) {
-      abort(`--directory missing or not a directory: ${dir}`);
-    }
-    Deno.chdir(dir);
-  }
-  if (!path.isAbsolute(drakefile)) {
-    drakefile = path.join(Deno.cwd(), drakefile);
-  }
-  import(drakefile);
+  import(env["--drakefile"]);
 }
--- mod.ts
+++ mod.ts
@@ -4,6 +4,8 @@ const vers = "0.1.0";
 export { abort, glob, quote, sh } from "./lib/utils.ts";
 export { desc, execute, run, task, log, env, vers };
 
+import { existsSync } from "https://deno.land/std@v0.33.0/fs/mod.ts";
+import * as path from "https://deno.land/std@v0.33.0/path/mod.ts";
 import { Env, parseArgs } from "./lib/cli.ts";
 import { help } from "./lib/help.ts";
 import { TaskRegistry } from "./lib/tasks.ts";
@@ -27,11 +29,32 @@ for (const name of Object.getOwnPropertyNames(Deno.env())) {
   env[`$${name}`] = Deno.env(name);
 }
 
-/** Global task registry. */
-const taskRegistry = new TaskRegistry(env);
-
 // Parse command-line options into Drake environment.
 parseArgs(Deno.args.slice(), env);
+
+if (env["--help"]) {
+  help();
+} else if (env["--version"]) {
+  console.log(vers);
+}
+
+// Caclulate drakefile path relative to cwd prior to processing --directory option.
+let drakefile = env["--drakefile"] ? env["--drakefile"] : "./Drakefile.ts";
+if (!path.isAbsolute(drakefile)) {
+  drakefile = path.join(Deno.cwd(), drakefile);
+}
+env["--drakefile"] = drakefile;
+
+if (env["--directory"]) {
+  const dir = env["--directory"];
+  if (!existsSync(dir) || !Deno.statSync(dir).isDirectory()) {
+    abort(`--directory missing or not a directory: ${dir}`);
+  }
+  Deno.chdir(dir);
+}
+
+/** Global task registry. */
+const taskRegistry = new TaskRegistry(env);
 
 /** Set description of next registered task. */
 function desc(description: string): void {
@@ -57,11 +80,10 @@ function log(message: string): void {
  * command-line targets are run or the default target is run.
  */
 async function run(...targets: string[]) {
-  if (env["--help"]) {
-    help();
-  } else if (env["--version"]) {
-    console.log(vers);
-  } else if (env["--tasks"]) {
+  if (env["--help"] || env["--version"]) {
+    return;
+  }
+  if (env["--tasks"]) {
     taskRegistry.list();
   } else {
     if (targets.length === 0) {
