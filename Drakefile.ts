ec07939b8255ef2a22f59fe82caa41bb88a49f7d
Stuart Rackham <srackham@gmail.com>
2020-02-28 17:54:59 +1300

    build: Added `push` and `tag` tasks.

--- Drakefile.ts
+++ Drakefile.ts
@@ -1,4 +1,4 @@
-import { desc, env, glob, quote, run, sh, task } from "./mod.ts";
+import { abort, desc, env, glob, quote, run, sh, task } from "./mod.ts";
 
 env["--default-task"] = "test";
 const SRC_FILES = glob("**/*.ts");
@@ -24,6 +24,26 @@ task("run", ["test"], async function() {
     cd ./examples
     deno run -A ./Drakefile.ts prereqs pause "qux=Foo Bar" noop
   `);
+});
+
+desc(
+  "Create Git version tag e.g. 'drake tag vers=1.0.0' creates tag 'v1.0.0'"
+);
+task("tag", ["test"], async function() {
+  if (!env.vers) {
+    abort("'vers' command-line variable not set e.g. drake tag vers=1.0.0");
+  }
+  if (!/^\d+\.\d+\.\d+/.test(env.vers as string)) {
+    abort(`illegal semantic version number: ${env.vers}`);
+  }
+  const tag = `v${env.vers}`;
+  console.log(`tag: ${tag}`);
+  await sh(`git tag -a -m ${tag} ${tag}`);
+});
+
+desc("Push changes to Github");
+task("push", ["test"], async function() {
+  await sh("git push -u --tags origin master");
 });
 
 run();
