0e64fc12044d8f6688ff7809b2e67f098fd19bfb
Stuart Rackham <srackham@gmail.com>
2020-03-12 11:46:26 +1300

    test: More drake CLI tests.

--- tests/cli_test.ts
+++ tests/cli_test.ts
@@ -1,5 +1,6 @@
 import {
-  assertEquals
+  assertEquals,
+  assertStrContains
 } from "https://deno.land/std@v0.35.0/testing/asserts.ts";
 import { env, shCapture, vers } from "../mod.ts";
 
@@ -8,7 +9,51 @@ env["--abort-exits"] = false;
 Deno.test(
   async function cliTest() {
     const drake = "deno run -A drake.ts";
-    const { stdout } = await shCapture(`${drake} --version`);
+    let { code, stdout, stderr } = await shCapture(`${drake} --version`);
+    assertEquals(code, 0);
     assertEquals(stdout.trimRight(), vers);
+    assertEquals(stderr, "");
+
+    ({ code, stdout, stderr } = await shCapture(`${drake} --help`));
+    assertEquals(code, 0);
+    assertStrContains(stdout, "drake - a make-like task runner for Deno.");
+    assertEquals(stderr, "");
+
+    ({ code, stdout, stderr } = await shCapture(
+      `${drake} --list-tasks`,
+      { env: { "NO_COLOR": "true" } }
+    ));
+    assertEquals(code, 0);
+    assertStrContains(stdout, "Push changes to Github [test]");
+    assertEquals(stderr, "");
+
+    ({ code, stdout, stderr } = await shCapture(`${drake} --foobar`));
+    assertEquals(code, 1);
+    assertEquals(stdout, "");
+    assertStrContains(stderr, "illegal option: --foobar");
+
+    ({ code, stdout, stderr } = await shCapture(`${drake} nonexistent-task`));
+    assertEquals(code, 1);
+    assertEquals(stdout, "");
+    assertStrContains(stderr, "missing task: nonexistent-task");
+
+    ({ code, stdout, stderr } = await shCapture(`${drake} nonexistent-task`));
+    assertEquals(code, 1);
+    assertEquals(stdout, "");
+    assertStrContains(stderr, "missing task: nonexistent-task");
+
+    ({ code, stdout, stderr } = await shCapture(
+      `${drake} -f missing-drakefile.ts`
+    ));
+    assertEquals(code, 1);
+    assertEquals(stdout, "");
+    assertStrContains(stderr, "--drakefile missing or not a regular file");
+
+    ({ code, stdout, stderr } = await shCapture(
+      `${drake} -d missing-directory`
+    ));
+    assertEquals(code, 1);
+    assertEquals(stdout, "");
+    assertStrContains(stderr, "--directory missing or not a directory");
   }
 );
