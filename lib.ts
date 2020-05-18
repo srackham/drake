8dd3fd3d63d9f64427cc45a8b659fba2cabe6b30
Stuart Rackham <srackham@gmail.com>
2020-05-19 08:18:29 +1200

    feature: Make APIs available to non-drakefile with `lib.ts`.

---
+++ lib.ts
@@ -0,0 +1,18 @@
+/* APIs that can be used in non-Drake modules. */
+export { env } from "./lib/env.ts";
+export {
+  abort,
+  debug,
+  DrakeError,
+  glob,
+  log,
+  quote,
+  readFile,
+  sh,
+  shCapture,
+  ShCaptureOpts,
+  ShOpts,
+  ShOutput,
+  updateFile,
+  writeFile,
+} from "./lib/utils.ts";
