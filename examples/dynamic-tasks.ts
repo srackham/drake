bad41c7e12aeb4b287a7d4a1262f38d34b8e2110
Stuart Rackham <srackham@gmail.com>
2020-06-27 18:24:29 +1200

    examples: Add default task to dynamic tasks example.

--- examples/dynamic-tasks.ts
+++ examples/dynamic-tasks.ts
@@ -1,12 +1,20 @@
-import * as path from "https://deno.land/std@v0.58.0/path/mod.ts";
+import * as path from "https://deno.land/std@v0.59.0/path/mod.ts";
+import { env } from "../lib/env.ts";
 import { desc, glob, run, sh, task } from "../mod.ts";
+
+const tasks: string[] = [];
 
 for (const prereq of glob("*.md")) {
   const target = `${path.basename(prereq, ".md")}.html`;
   desc(`compile "${target}"`);
   task(target, [prereq], async function () {
-    await sh(`markdown "${prereq}" > "${target}"`);
+    await sh(`marked "${prereq}" > "${target}"`);
   });
+  tasks.push(target);
 }
+
+desc("compile markdown");
+task("compile-md", tasks);
+env("--default-task", "compile-md");
 
 run();
