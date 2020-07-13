b6dd7a3da7e7c2331221efc23e1d9c3a658ea219
Stuart Rackham <srackham@gmail.com>
2020-07-14 11:38:33 +1200

    Bump version number.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { colors, existsSync, path, walkSync } from "./deps.ts";
 import { env } from "./env.ts";
 
-const DRAKE_VERS = "1.2.4";
+const DRAKE_VERS = "1.2.5";
 
 /** Returns the Drake version number string. */
 export function vers(): string {
