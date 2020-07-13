7f967bed1b32059fc9ec778e26a380381bec8973
Stuart Rackham <srackham@gmail.com>
2020-07-14 11:38:07 +1200

    upgrade: Deno 1.2.0, std@v0.61.0.

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,5 +1,5 @@
-export { existsSync } from "https://deno.land/std@v0.60.0/fs/exists.ts";
-export * as path from "https://deno.land/std@v0.60.0/path/mod.ts";
+export { existsSync } from "https://deno.land/std@v0.61.0/fs/exists.ts";
+export * as path from "https://deno.land/std@v0.61.0/path/mod.ts";
 export {
   assert,
   assertEquals,
@@ -7,4 +7,4 @@ export {
   assertStringContains,
   assertThrows,
   assertThrowsAsync,
-} from "https://deno.land/std@v0.60.0/testing/asserts.ts";
+} from "https://deno.land/std@v0.61.0/testing/asserts.ts";
