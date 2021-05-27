056c0c77e88978fa13d394bf2330837096a24635
Stuart Rackham <srackham@gmail.com>
2021-05-27 13:44:23 +1200

    build: bump version number to 1.5.0

--- examples/minimal-drakefile.ts
+++ examples/minimal-drakefile.ts
@@ -1,5 +1,5 @@
-import { desc, run, task } from "https://deno.land/x/drake@v1.4.7/mod.ts";
-// import { desc, run, task } from "https://x.nest.land/drake@1.4.7/mod.ts";
+import { desc, run, task } from "https://deno.land/x/drake@v1.5.0/mod.ts";
+// import { desc, run, task } from "https://x.nest.land/drake@1.5.0/mod.ts";
 
 desc("Minimal Drake task");
 task("hello", [], function () {
