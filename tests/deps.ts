56125223103ee66de5153e5117749f3c0b72a719
Stuart Rackham <srackham@gmail.com>
2022-01-26 10:14:44 +1300

    build: update Deno libs to std@v0.122.0, Deno 1.18.0

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,5 +1,5 @@
-export { existsSync } from "https://deno.land/std@0.119.0/fs/exists.ts";
-export * as path from "https://deno.land/std@0.119.0/path/mod.ts";
+export { existsSync } from "https://deno.land/std@0.122.0/fs/exists.ts";
+export * as path from "https://deno.land/std@0.122.0/path/mod.ts";
 export {
   assert,
   assertEquals,
@@ -7,4 +7,4 @@ export {
   assertRejects,
   assertStringIncludes,
   assertThrows,
-} from "https://deno.land/std@0.119.0/testing/asserts.ts";
+} from "https://deno.land/std@0.122.0/testing/asserts.ts";
